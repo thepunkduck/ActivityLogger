8c93f5b474d6d2e51f9e2fda1bdcc0f74a8c3b3e
James Parsons <jparsons68@yahoo.com.au>
2020-01-31 18:19:52 +1100

    Add project files.

---
+++ ActivityLogger/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ActivityLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FOLLOWING";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(261, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(474, 194);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ActivityLogger/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActivityLogger
+{
+    public partial class Form1 : Form
+    {
+        List<Point> ptList = new List<Point>();
+        Timer timer;
+        Point lastMousePostion;
+        float scaleF;
+        public Form1()
+        {
+            InitializeComponent();
+
+            int x0 = 99999;
+            int x1 = 0;
+            int y0 = 99999;
+            int y1 = 0;
+            foreach (var scrn in Screen.AllScreens)
+            {
+                x0 = Math.Min(x0, scrn.Bounds.Left);
+                x1 = Math.Max(x1, scrn.Bounds.Right);
+                y0 = Math.Min(y0, scrn.Bounds.Top);
+                y1 = Math.Max(y1, scrn.Bounds.Bottom);
+            }
+
+            double prop = ((double)(y1 - y0)) / (x1 - x0);
+            panel1.Height = (int)(panel1.Width * prop);
+
+            scaleF = ((float)panel1.Width) / (x1 - x0);
+
+            lastMousePostion = new Point(0, 0);
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var pos = MousePosition;
+            if (!pos.Equals(lastMousePostion))
+            {
+                ptList.Add(pos);
+                lastMousePostion = pos;
+
+                StringBuilder sb = new StringBuilder();
+
+                int a = ptList.Count - 10;
+                if (a < 0) a = 0;
+                for (int i = a; i < ptList.Count; i++)
+                {
+                    sb.AppendLine(i.ToString() + " : " + ptList[i].ToString());
+                }
+
+                label1.Text = sb.ToString();
+                panel1.Invalidate();
+            }
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.ResetTransform();
+            e.Graphics.ScaleTransform(scaleF, scaleF);
+
+
+            Brush br = new SolidBrush(Color.FromArgb(50, Color.Red));
+            float sz = 4f / scaleF;
+            for (int i = 1; i< ptList.Count; i++)
+            {
+                var pt = ptList[i];
+                var pt1 = ptList[i - 1];
+                //e.Graphics.DrawLine(Pens.Yellow, pt, pt1);
+                e.Graphics.FillRectangle(br, pt.X, pt.Y, sz, sz);
+            }
+
+        }
+    }
+}
